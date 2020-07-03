bee4fbcb84b32da75d473f4f86c609429580f15d
Karolina Jimenez <karojg@gmail.com>
2020-07-02 21:44:21 -0600

    feature/mainNavigation: Creates Main page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Layout, Menu } from 'antd';
+import 'antd/dist/antd.css';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import About from './components/About';
 
-function App() {
+const { Header, Content, Footer } = Layout;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className="layout">
+      <Router>
+        <Header>
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1">
+              <Link to="/about">About</Link>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Link to="/position">Position</Link>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Link to="/display">Display</Link>
+            </Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <div className="site-layout-content">
+            <Switch>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/position">POSITION</Route>
+              <Route path="/display">DISPLAY</Route>
+            </Switch>
+          </div>
+        </Content>
+      </Router>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    </Layout>
   );
-}
+};
 
 export default App;
